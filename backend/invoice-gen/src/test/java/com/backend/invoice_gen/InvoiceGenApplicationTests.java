d5074717bce2b297c3960a23106e3f59a3b93257
Sai Deepak Perumbudur <psaideepak1@gmail.com>
2025-02-16 22:14:41 +0530

    Initialize project structure with backend and frontend setup

---
+++ backend/invoice-gen/src/test/java/com/backend/invoice_gen/InvoiceGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.invoice_gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
